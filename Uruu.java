c1f6cf0770327368897bab9165faadd29c223a7f
y-arimura1222 <y.arimura.carecon@gmail.com>
2021-12-05 22:02:53 +0900

    うるう年判定処理追加

--- Uruu.java
+++ Uruu.java
@@ -4,9 +4,20 @@ class Uruu {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 
-		System.out.println("検査したい西暦を入力:");
-		String year = scan.nextLine();
+		System.out.println("西暦を入力してください :");
+		Integer year = scan.nextInt();
 
-		System.out.println(year + "はうるう年です");
+		if (year <= 0) {
+			System.out.println("西暦を正確に入力してください");
+			return;
+		}
+
+		if (year % 100 == 0 && year % 400 != 0) {
+			System.out.println(year + "年は平年です");
+		} else if (year % 4 == 0) {
+			System.out.println(year + "年はうるう年です");
+		} else {
+			System.out.println(year + "年は平年です");
+		}
 	}
 }
