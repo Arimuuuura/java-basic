1813e111d1c43b01c7c84145ed71216b8a150b75
y-arimura1222 <y.arimura.carecon@gmail.com>
2021-12-10 01:04:56 +0900

    Java Basic 3 確認問題

---
+++ Lesson3_2.java
@@ -0,0 +1,31 @@
+class Lesson3_2 {
+    public static void main(String[] args) {
+        // No.1
+        int salary = 300000;
+        long worldPopulation = 7500000000L;
+        short diceNum = 4;
+        char bloodType = 'A';
+        float time = 9.87F;
+
+        System.out.println(salary);
+        System.out.println(worldPopulation);
+        System.out.println(diceNum);
+        System.out.println(bloodType);
+        System.out.println(time);
+
+        // No.2
+        byte b = 0;
+        int i = 10;
+        b = (byte)i;
+
+        System.out.println("b = " + b);
+        System.out.println("i = " + i);
+
+        float f = 1.0F;
+        double d = 3.14159;
+        f = (float)d;
+
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+    }
+}
