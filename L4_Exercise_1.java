a78210a3513dec5366ba292c91f268f2a60f1646
y-arimura1222 <y.arimura.carecon@gmail.com>
2021-12-21 00:09:28 +0900

    キーボード入力から得た値の型指定変更

--- L4_Exercise_1.java
+++ L4_Exercise_1.java
@@ -4,9 +4,9 @@ class L4_Exercise_1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("底辺は(cm)？");
-        double bottom = scan.nextInt();
+        double bottom = scan.nextDouble();
         System.out.println("高さは(cm)？");
-        double height = scan.nextInt();
+        double height = scan.nextDouble();
 
         double area = (bottom * height) / 2;
         System.out.println("面積は" + area + "㎠");
