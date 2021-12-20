b062afc54a9a5d631470356f82b46cc4299bddd6
y-arimura1222 <y.arimura.carecon@gmail.com>
2021-12-21 00:36:11 +0900

    四番目の言語を追加

--- L5_Exercise_2.java
+++ L5_Exercise_2.java
@@ -9,7 +9,9 @@ public class L5_Exercise_2 {
         String second = scan.next();
         System.out.println("言語を入力");
         String third = scan.next();
-        String[] array = new String[]{first, second, third};
-        System.out.println("言語1:" + array[0] + "," + "言語2:" + array[1] + "," + "言語3:" + array[2]);
+        System.out.println("言語を入力");
+        String fourth = scan.next();
+        String[] array = new String[]{first, second, third, fourth};
+        System.out.println("言語1:" + array[0] + "," + "言語2:" + array[1] + "," + "言語3:" + array[2] + "," + "言語3:" + array[3]);
     }
 }
