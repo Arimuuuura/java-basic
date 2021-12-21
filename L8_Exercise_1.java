57b89480103256dfebbdfa01b320ff20e335682e
y-arimura1222 <y.arimura.carecon@gmail.com>
2021-12-21 22:56:04 +0900

    インデントを整えた

--- L8_Exercise_1.java
+++ L8_Exercise_1.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class L8_Exercise_1 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		System.out.println("割り算をします。( a / b)");
+		System.out.println("割り算をします。( a / b )");
 		System.out.println("a の値を入力してください");
 		Double num1 = scan.nextDouble();
 		System.out.println("b の値を入力してください");
